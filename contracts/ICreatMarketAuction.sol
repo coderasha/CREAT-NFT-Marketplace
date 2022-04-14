5eac3fd4e50ac3a037eb8c5b7083879df18af8cc
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2022-04-14 06:30:01 +0530

    Update ICreatMarketAuction.sol

--- contracts/ICreatMarketAuction.sol
+++ contracts/ICreatMarketAuction.sol
@@ -5,7 +5,7 @@ pragma solidity 0.6.12;
 interface ICreatMarketAuction {
     /**
      * @dev Set the token for sale. The owner of the token must be the sender and have the marketplace approved.
-     * @param _tokenId uint256 ID of the token
+   
      * @param _amount uint256 wei value that the item is for sale
      */
     function setSalePrice(
